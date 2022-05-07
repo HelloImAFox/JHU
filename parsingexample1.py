cbb4a1fd37d60559f37077f48e82597d65ca9864
HelloImAFox <84694832+HelloImAFox@users.noreply.github.com>
2022-05-06 21:33:37 -0700

    lets see if we can revert

--- parsingexample1.py
+++ parsingexample1.py
@@ -3,12 +3,13 @@ with open('/users/christopherknuff/desktop/mbox-short.txt') as file:
 #    line = file.readline()
     for line in file:
         line.rstrip()
-        if line == ' ': #this skips blank lines to prevent traceback
+        if line == '': #this skips blank lines to prevent traceback
             continue
-   #to look for a specific word in a line
+#This also skips blank lines
         words = line.split()
         if len(words) < 1:
             continue
+# to look for a specific word in a line
         if not words[0] == 'From': #if first word doesnt = 0
             continue
         print(words[2])
