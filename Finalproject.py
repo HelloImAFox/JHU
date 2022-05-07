c8c5beb11dec4425654f022f1a9140198bf72959
HelloImAFox <84694832+HelloImAFox@users.noreply.github.com>
2022-05-07 12:17:30 -0700

    This currently reads all lines and prints from the command line with ./Finalproject.py -i Saccharomyces_cerevisiae_S288C.annotation.gff

--- Finalproject.py
+++ Finalproject.py
@@ -7,5 +7,13 @@ mypar = argparse.ArgumentParser()
 mypar.add_argument('-i', '--usrinput', type = str, required = True, help = 'I hope this works')
 args = mypar.parse_args()
 #Try to parse through the file
-for line in open(args.usrinput):
-    print(line)
+for lin in open(args.usrinput):
+    for word in lin:
+        if not 'Parent=YAR003W' in lin:
+            continue
+        else:
+            print(lin)
+
+        #if not 'CDS' in lin:
+         #   continue
+        #print(lin)
--- finalbackup1.py
+++ finalbackup1.py
@@ -8,5 +8,21 @@ mypar = argparse.ArgumentParser()
 mypar.add_argument('-i', '--usrinput', type = str, required = True, help = 'I hope this works')
 args = mypar.parse_args()
 #Try to parse through the file
-for line in open(args.usrinput):
-    print(line)
+for lin in open(args.usrinput):
+    lin.rstrip()
+    print(lin)
+######
+#!/usr/bin/env python3
+import argparse
+import sys
+import re
+import os
+mypar = argparse.ArgumentParser()
+mypar.add_argument('-i', '--usrinput', type = str, required = True, help = 'I hope this works')
+args = mypar.parse_args()
+#Try to parse through the file
+for lin in open(args.usrinput):
+    lin.strip()
+    for word in lin:
+        if not 'ID=YAR003W' in lin:
+            continue
