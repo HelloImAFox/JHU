1e84271eda2656ed750da2279218208cafddea4c
HelloImAFox <84694832+HelloImAFox@users.noreply.github.com>
2022-05-07 00:17:38 -0700

    lets see if we can revert

--- dictionary3.py
+++ dictionary3.py
@@ -1,10 +1,10 @@
 #using dictionaries part 2
 dictionary = {}
 words = input('Please enter some lines: ')
-words = words.strip('"')
-words = words.split()
-for word in words :
+zork = words.replace('"', '')
+zorg = zork.split()
+for word in zorg :
     dictionary[word] = dictionary.get(word, 0) + 1
 print(dictionary)
-for key in dictionary :
-    print(key, dictionary[key])
+for blah in dictionary :
+    print(blah, dictionary[blah])
